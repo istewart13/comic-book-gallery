4f369d82a68d3297f04fed382890eb1c2c7a1d2d
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-19 17:04:11 +0100

    add all stuff

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        private int Id { get; set; }
+        private string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public string[] Artists { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
