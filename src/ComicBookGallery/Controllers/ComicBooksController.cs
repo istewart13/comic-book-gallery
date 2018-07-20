7eb91cc6a54c8be652152c0a20a00f8818d21efd
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-07-20 16:04:13 +0100

    Create comic book repo object

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
         // GET: ComicBooks
         public ActionResult Index()
         {
